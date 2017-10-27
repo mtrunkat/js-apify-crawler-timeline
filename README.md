9984f7bf438d7471733645670657218c418b6469
Marek Trunkat <marek@trunkat.eu>
2017-10-27 12:01:37 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# apify-crawler-to-timeline
+# Apify Crawler Timeline
+
 This act creates a timeline spreadsheet from crawler results. Main usecase is to create a spreadsheet containing changes of some web page in time.
+
+Crawler has to satisfy:
+
+* Returns exactly one page
+* Page function result for that page is simple object
+
+For example if output of your crawler is ...
+
+```json
+{
+   someString: 'something',
+   someNumber: 123.456
+}
+```
+
+... and you set this act as finish webhook of that crawler then it creates a key-value store with the name of your crawler and generates following table there in `json` and `csv` formats:
+
+| Date       | 5.6.2017 22:00:00       | 5.6.2017 23:00:00 | 6.6.2017 00:00:00 |
+|------------|-------------------------|-------------------|-------------------|
+| someString | some value at that date | some other value  | some other value  |
+| someNumber | 123.4                   | 42                | 1                 |
+
+On each run of your crawler the table gets updated. 
---
+++ main.js
@@ -0,0 +1,91 @@
+const Apify = require('apify');
+const request = require('request-promise');
+const _ = require('underscore');
+const toCSV = require('array-to-csv')
+
+Apify.main(async () => {
+    const { crawlers, keyValueStores } = Apify.client;
+    const input = await Apify.getValue('INPUT');
+    const { _id, actId } = input;
+
+    console.log('INPUT:');
+    console.log(input);
+
+    if (!_id) throw new Error('"_id" field is missing in INPUT!');
+    if (!actId) throw new Error('"actId" field is missing in INPUT!');
+
+    const crawler = await crawlers.getCrawlerSettings({
+        crawlerId: actId,
+    });
+
+    if (!crawler) throw new Error('Crawler not found!');
+
+    console.log('Crawler:');
+    console.log(crawler);
+
+    const store = await keyValueStores.getOrCreateStore({
+        storeName: crawler.customId.replace(/\W+/g, '-'),
+    });
+
+    console.log('Store:');
+    console.log(store);
+
+    const result = await crawlers.getExecutionResults({
+        executionId: _id,
+    });
+
+    console.log('Result:');
+    console.log(result);
+
+    if (!result || !result.items || !result.items.length) throw new Error('Result is empty!');
+    if (result.items.length !== 1) throw new Error('Result must contain exactly one page!');
+
+    const pageFunctionResult = result.items[0].pageFunctionResult;
+
+    if (!_.isObject(pageFunctionResult)) throw new Error('PageFunction result must be object!');
+    if (!_.keys(pageFunctionResult).length) throw new Error('PageFunction is empty!');
+
+    console.log('Page function result:');
+    console.log(pageFunctionResult);
+
+    const record = await keyValueStores.getRecord({
+        key: 'timeline.json',
+        storeId: store.id,
+    });
+
+    const timeline = record
+        ? record.body
+        : [['Date']].concat(_.keys(pageFunctionResult).map(key => [key]));
+
+    pageFunctionResult['Date'] = new Date(result.items[0].pageFunctionFinishedAt);
+
+    const updateTimeline = timeline.map((line) => {
+       const key = line[0];
+       const newVal = pageFunctionResult[key];
+
+       delete pageFunctionResult[key];
+
+       return line.concat(newVal);
+    });
+
+    console.log('Timeline:');
+    console.log(timeline);
+    console.log('Updated timeline:');
+    console.log(updateTimeline);
+
+    await keyValueStores.putRecord({
+        key: 'timeline.json',
+        contentType: 'application/json',
+        body: JSON.stringify(updateTimeline),
+        storeId: store.id,
+    });
+
+    await keyValueStores.putRecord({
+        key: 'timeline.csv',
+        contentType: 'application/vnd.ms-excel',
+        body: toCSV(updateTimeline),
+        storeId: store.id,
+    });
+
+    console.log('Done!');
+});
